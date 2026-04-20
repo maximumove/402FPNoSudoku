4a8cf5dca06125ca1b2e97434beac8fc68462552
Troy Berhow <troyberhow@u.boisestate.edu>
2026-04-20 15:48:01 -0600

    Ran with npx expo start, moved js files into app folder

--- NoSudoku/index.js
+++ NoSudoku/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 import { StyleSheet, View, Text, Button } from 'react-native';
-import HomeScreen from './homeScreen';
+import HomeScreen from './app/(tabs)/homeScreen';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
